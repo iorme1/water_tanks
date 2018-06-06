b1eeb8a9ded8324cbd47b562902ae67c2cc1faf1
Isaac Orme <isorme1@gmail.com>
2018-06-06 14:24:32 -0700

    fixes mixed up ids

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,5 @@
 class PhotosController < ApplicationController
-  before_action :set_tank_location, :only => [ :create ]
+  before_action :set_tank_location, :only => [ :create, :destroy ]
 
   def new
     @photo = current_user.photos.build
@@ -17,8 +17,7 @@ class PhotosController < ApplicationController
   end
 
   def destroy
-    @tank_location = TankLocation.find(params[:id])
-    @photo = @tank_location.photos.find(params[:tank_location_id])
+    @photo = @tank_location.photos.find(params[:id])
     @photo.destroy
 
     flash[:notice] = "Photo has been removed."
